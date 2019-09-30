f7b17715fdec5c749e87aed2b42035562dcc6206
Tercio Jose <terciob19@hotmail.com>
2019-09-30 15:28:17 -0300

    adding !faq for Details category

--- bot-core.js
+++ bot-core.js
@@ -1,22 +1,27 @@
 
 //grab the discord 'library'
-const Discord = require ('discord.js');
+const Discord = require ("discord.js");
 //make the bot client object
 const botClient = new Discord.Client();
 
 //bot goes online
-botClient.on('ready', () => {
-    //console.log('!');
+botClient.on("ready", () => {
+
 })
 
 //events from the discord server
-botClient.on('message', msg=> {
-    switch (msg.content)
+botClient.on("message", msg=> {
+    switch (msg.channel.parent)
     {
-        case "!test":
-            msg.reply("test test");
-            break;
+        case "Details! Damage Meter":
+            switch (msg.content)
+            {
+                case "!faq":
+                    msg.channel.send("https://www.curseforge.com/wow/addons/details/pages/faq")
+                    break;
+            }
     }
+
 })
 
 //token for the login process
