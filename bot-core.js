30d5bc83bd8e9f7c30510c52d613ba490760badb
Continuity <continuity@gmx.net>
2021-03-15 23:23:38 +0100

    - it was async all along

--- bot-core.js
+++ bot-core.js
@@ -143,7 +143,7 @@ botClient.on('message', function (message)
 botClient.on('guildBanAdd', async (guild, user) => {
     //console.log(`guildBanAdd: ${guild}, ${user}`);
     try {
-        const logs = botClient.channels.fetch(modLogChannel);
+        const logs = await botClient.channels.fetch(modLogChannel).fetch();
         const fetchedLogs = await guild.fetchAuditLogs({
             limit: 1,
             type: 'MEMBER_BAN_ADD',
@@ -165,7 +165,7 @@ botClient.on('guildBanAdd', async (guild, user) => {
 botClient.on('guildMemberRemove', async (member) => {
     //console.log(`guildMemberRemove: ${guild}, ${user}`);
     try {
-        const logs = botClient.channels.fetch(modLogChannel);
+        const logs = await botClient.channels.fetch(modLogChannel);
         const fetchedLogs = await guild.fetchAuditLogs({
             limit: 1,
             type: 'MEMBER_KICK',
