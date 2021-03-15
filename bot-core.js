b7fd7c532210ee3c7fc7a9909021a0864cc3514d
Continuity <continuity@gmx.net>
2021-03-15 23:23:54 +0100

    - no! fix!

--- bot-core.js
+++ bot-core.js
@@ -143,7 +143,7 @@ botClient.on('message', function (message)
 botClient.on('guildBanAdd', async (guild, user) => {
     //console.log(`guildBanAdd: ${guild}, ${user}`);
     try {
-        const logs = await botClient.channels.fetch(modLogChannel).fetch();
+        const logs = await botClient.channels.fetch(modLogChannel);
         const fetchedLogs = await guild.fetchAuditLogs({
             limit: 1,
             type: 'MEMBER_BAN_ADD',
