0d9fe9fea94a21043c1590e1e01492083e1e4c30
cont1nuity <continuity@gmx.net>
2025-01-27 16:37:55 +0100

    - this should be better

--- bot-core.js
+++ bot-core.js
@@ -84,14 +84,17 @@ function incrementBanCounter(channel)
 function banUserForSpam(user, message, channel)
 {
     // Ban a user by id (or with a user/guild member object)
-    user.guild.bans.create(user, { deleteMessageSeconds: 6 * 60 * 60, reason: `#spam-bot-bait: ${message}` }); //6h
+    user.guild.bans.bulkCreate([user], { deleteMessageSeconds: 6 * 60 * 60, reason: `#spam-bot-bait: ${message}` }); //6h
+    .then(result => {
+        console.log(`Banned ${result.bannedUsers.length} users, failed to ban ${result.failedUsers.length} users.`);
+        if (result.bannedUsers.length > 0) incrementBanCounter(channel);
+    })
     .catch( async (err) => {
             const logs = await botClient.channels.fetch(modLogChannel);
             const embed = createErrorEmbed('Issue during Bot Spam handling', err.stack);
             logs.send({ embeds: [embed] }).catch((err) => { console.log(err) })
         }
     );
-    incrementBanCounter(channel);
 }
 
 function handleBotSpamChannel(message)
